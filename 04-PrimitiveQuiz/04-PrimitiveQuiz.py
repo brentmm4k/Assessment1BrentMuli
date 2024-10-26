d8233d591b1df7d7c63feaa64150279160099465
Brent Muli <brentmarvel.muli@gmail.com>
2024-10-26 23:12:40 +0400

    Create 04-PrimitiveQuiz.py

---
+++ 04-PrimitiveQuiz/04-PrimitiveQuiz.py
@@ -0,0 +1,45 @@
+answer1 = input('What is the capital of France? ').lower()
+if answer1 == 'paris': print('You are correct!')
+else: print('You are incorrect')
+
+answer2 = input('What is the capital of the UK? ').lower()
+if answer2 == 'london': print('You are correct!')
+else: print('You are incorrect')
+
+answer3 = input('What is the capital of Spain? ').lower()
+if answer3 == 'madrid': print('You are correct!')
+else: print('You are incorrect')
+
+answer4 = input('What is the capital of Sweden? ').lower()
+if answer4 == 'stockholm': print('You are correct!')
+else: print('You are incorrect')
+
+answer5 = input('What is the capital of Iceland? ').lower()
+if answer5 == 'reykjavik': print('You are correct!')
+else: print('You are incorrect')
+
+answer6 = input('What is the capital of Belgium? ').lower()
+if answer6 == 'brussels': print('You are correct!')
+else: print('You are incorrect')
+
+answer6 = input('What is the capital of Italy? ').lower()
+if answer6 == 'rome': print('You are correct!')
+else: print('You are incorrect')
+
+answer7 = input('What is the capital of Germany? ').lower()
+if answer7 == 'berlin': print('You are correct!')
+else: print('You are incorrect')
+
+answer8 = input('What is the capital of Netherlands? ').lower()
+if answer8 == 'amsterdam': print('You are correct!')
+else: print('You are incorrect')
+
+answer9 = input('What is the capital of Poland? ').lower()
+if answer9 == 'warsaw': print('You are correct!')
+else: print('You are incorrect')
+
+answer10 = input('What is the capital of Russia? ').lower()
+if answer10 == 'moscow': print('You are correct!')
+else: print('You are incorrect')
+
+# The ".lower()" coverts any input all lowercase, which would always result in the same capitalization as the correct answer.
