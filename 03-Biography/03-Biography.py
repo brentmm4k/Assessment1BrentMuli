3e41b50bacc77730cdb2473ff99f8aa49c54d85f
Brent Muli <brentmarvel.muli@gmail.com>
2024-10-26 22:51:52 +0400

    Create 03-Biography.py

---
+++ 03-Biography/03-Biography.py
@@ -0,0 +1,10 @@
+name = input('Please enter your name: ')
+ht = input('Please enter your hometown: ')
+age = int(input('Please enter your age: '))
+
+print(f'Name: {name}')
+print(f'Hometown: {ht}')
+print(f'Age: {age}')
+
+# Stores the data of name, hometown, and age into variabes, then prints them in the console. 
+# To prevent letters from being inputted in the "age" variable, the "int" data type has been implemented
