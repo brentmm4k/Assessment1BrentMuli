a01c6cb74958b1f2f2ab989f2df4ddbb8f91cd9f
Brent Muli <brentmarvel.muli@gmail.com>
2024-10-26 22:42:03 +0400

    Create 02-SimpleSums.py

---
+++ 02-SimpleSums/02-SimpleSums.py
@@ -0,0 +1,7 @@
+num1 = int(8)
+num2 = int(10)
+sum = num1 + num2
+
+print(sum)
+
+#Stores 2 numbers in variables("num1" and "num2") to be added in the "sum" variable, then the result is printed in the console.
