04404f957f29e7db947cd3c41bd4b65204fbadbb
Brent Muli <brentmarvel.muli@gmail.com>
2024-10-28 10:43:26 +0400

    Create 07-SomeCounting.py

---
+++ 07-SomeCounting/07-SomeCounting.py
@@ -0,0 +1,27 @@
+loop1 = 0
+while loop1 <= 50:
+    loop1 += 1
+    print(loop1)
+
+loop2 = 50
+while loop2 >= 0:
+    loop2 -= 1
+    print(loop2)
+    if loop2 == 0: break
+
+loop3 = 30
+while loop3 <= 50:
+    loop3 += 1
+    print(loop3)
+    
+loop4 = 50
+while loop4 >= 10:
+    loop4 -= 2
+    print(loop4)
+    if loop4 == 10: break
+
+loop5 = 100
+while loop5 <= 200:
+    loop5 += 5
+    print(loop5)
+    if loop5 == 200: break
