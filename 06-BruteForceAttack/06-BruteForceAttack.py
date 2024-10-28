83a23b99eb7845289967d471569c202355fc95ae
Brent Muli <brentmarvel.muli@gmail.com>
2024-10-28 10:29:58 +0400

    Update 06-BruteForceAttack.py

--- 06-BruteForceAttack/06-BruteForceAttack.py
+++ 06-BruteForceAttack/06-BruteForceAttack.py
@@ -1,12 +1,17 @@
 correct_password = 12345
 attempt = 0
+max = 5
 
 password = int(input('You have 5 attempts. Please enter the password: '))
 while password != correct_password:
     attempt += 1
-    password = int(input(f'Try again! {5 - attempt} attempt/s left. Please enter the password: '))
-    if attempt == 5:
+    password = int(input(f'You have {5 - attempt} attempt/s. Please enter the password: '))
+    if attempt == max:
         print('You have 0 attempts left. The authorities have been alerted')
         break
     if password == correct_password:
         print ('You are correct')
+
+# The default attempts is set at 0. Whenever the password is incorrect, it adds 1 to the attempts until it reaches the max which is at 5
+# When the values or attempts and max varialbles are equal, it will inform the user that the authorities have been alerted.
+# The while loop only stops when the max attempts have been reached or the correct password has been inputted
