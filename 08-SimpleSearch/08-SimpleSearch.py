a1318b2e690e7878fc1d8aa9691096815fa81ca0
Brent Muli <brentmarvel.muli@gmail.com>
2024-10-28 11:18:50 +0400

    Update 08-SimpleSearch.py

--- 08-SimpleSearch/08-SimpleSearch.py
+++ 08-SimpleSearch/08-SimpleSearch.py
@@ -1,7 +1,9 @@
 list = ["Jake", "Zac", "Ian", "Ron", "Sam", "Dave"]
 
-search = input("Please type the name: ")
+search = input("Please type the name you want to search: ")
 if search in list:
     print(f'You have found {search}!')
 else: 
-    print('You have not found Sam')
+    print('Name not found')
+
+# Checks if the name inputted is in the list of strings, if it is, the program will print you have found {search}; their input.
