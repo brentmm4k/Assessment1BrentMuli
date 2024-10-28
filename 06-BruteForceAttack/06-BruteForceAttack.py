5d6ba5c3eab099fe40a30328537341b418426ea5
Brent Muli <brentmarvel.muli@gmail.com>
2024-10-28 10:03:37 +0400

    Update 06-BruteForceAttack.py

--- 06-BruteForceAttack/06-BruteForceAttack.py
+++ 06-BruteForceAttack/06-BruteForceAttack.py
@@ -5,13 +5,13 @@ password = int(input('You have 5 attempts left. Please enter the password: '))
 while password != correct_password:
     attempt += 1
     if attempt == 1:
-        password = int(input(f'You have {attempt} attempts left. Please enter the password: '))
+        password = int(input('You have 4 attempts left. Please enter the password: '))
     elif attempt == 2:
         password = int(input('You have 3 attempts left. Please enter the password: '))
     elif attempt == 3:
         password = int(input('You have 2 attempts left. Please enter the password: '))
     elif attempt == 4:
-        password = int(input('You have 1 attempts left. Please enter the password: '))
+        password = int(input('You have 1 attempt left. Please enter the password: '))
     elif attempt == 5:
         print('You have 0 attempts left. The authorities have been alerted')
     if password == correct_password:
