9797a9f6ad2ce69caa62d414367c7f3ba2b8186b
Brent Muli <brentmarvel.muli@gmail.com>
2024-10-28 10:44:57 +0400

    Update 07-SomeCounting.py

--- 07-SomeCounting/07-SomeCounting.py
+++ 07-SomeCounting/07-SomeCounting.py
@@ -2,6 +2,7 @@ loop1 = 0
 while loop1 <= 50:
     loop1 += 1
     print(loop1)
+    if loop1 == 50: break
 
 loop2 = 50
 while loop2 >= 0:
@@ -13,6 +14,7 @@ loop3 = 30
 while loop3 <= 50:
     loop3 += 1
     print(loop3)
+    if loop3 == 50: break
     
 loop4 = 50
 while loop4 >= 10:
